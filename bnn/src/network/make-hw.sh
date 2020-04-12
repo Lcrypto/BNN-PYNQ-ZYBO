bc858f658d33ae5e68bb3cfe157d1b1c257483b7
Franco Caspe <francocaspe@hotmmail.com>
2020-04-12 10:45:14 -0300

    Added humbleCNV model for Zybo-Z7.

--- bnn/src/network/make-hw.sh
+++ bnn/src/network/make-hw.sh
@@ -124,6 +124,12 @@ if [[ ("$MODE" == "h") || ("$MODE" == "a")  ]]; then
 	PARAMS="$XILINX_BNN_ROOT/../params/cifar10/$NETWORK"
 	TEST_INPUT="$XILINX_BNN_ROOT/../../tests/Test_image/deer.bin"
 	TEST_RESULT=4
+  elif [[ ("$NETWORK" == "hcnv"*) ]]; then
+	echo "WARNING: Due low accuracy, this small CNV does not classify correcly this image. True result is 4, but it should output 2. "
+    sleep 5
+    PARAMS="$XILINX_BNN_ROOT/../params/cifar10/$NETWORK"
+	TEST_INPUT="$XILINX_BNN_ROOT/../../tests/Test_image/deer.bin"
+	TEST_RESULT=2
   elif [[ ("$NETWORK" == "lfc"*) ]]; then
 	PARAMS="$XILINX_BNN_ROOT/../params/mnist/$NETWORK"
 	TEST_INPUT="$XILINX_BNN_ROOT/../../tests/Test_image/3.image-idx3-ubyte"
